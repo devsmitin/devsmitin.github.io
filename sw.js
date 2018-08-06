1ad2548f0204cbcb2d21c5a437799aaa294f8c2f
Smit Modi <imsmit@users.noreply.github.com>
2018-08-06 14:38:34 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
 importScripts('sw-toolbox.js');
-toolbox.precache(['index.html', 'style/style.css']);
-toolbox.router.get('/images/*', toolbox.cacheFirst);
+toolbox.precache(['index.html', 'https://imsmit.github.io/css-core/dist/css/core.min.css']);
+//toolbox.router.get('/images/*', toolbox.cacheFirst);
 toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
